8abbe2cb15e7205beb5b524ccd03d9e822fee110
sanya <sanec_290600@mail.ru>
2021-12-18 02:00:25 +1000

    [8]заебался

--- trash.py
+++ trash.py
@@ -1,17 +1,10 @@
 dict = {
     "key": "val",
     "key": "val",
-
-
-
     "key13": "val123",
-
-
     "key13": "val123",
-             "key13": "val123",
+    "key13": "val123",
     "key13": "val",
-
     "key13": "val",
-
     "key": "val121",
 }
