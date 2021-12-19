a417046fceb7bf2751ff404ac972dd1150c8cc2d
sanya <sanec_290600@mail.ru>
2021-12-19 16:27:06 +1000

    [11]я устал от всего

--- trash.py
+++ trash.py
@@ -2,11 +2,12 @@ dict = {
     "key": "val",
     "key": "val",
     "key13": "val123",
+
     "key13": "val123",
     "key13": "val123",
+
     "key13": "val123",
-    "key13": "val123",
-    "key13": "val123",
+
     "key13": "val",
     "key13": "val",
     "key": "val121",
